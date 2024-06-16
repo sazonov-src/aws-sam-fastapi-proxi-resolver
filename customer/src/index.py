b0c5d1d50061c7b7ed25cb6b988793a42fd79376
Vitalii Sazonov <vitaliy89vin@gmail.com>
2024-06-16 21:02:33 +0300

    add FastApi endpoint

--- customer/src/index.py
+++ customer/src/index.py
@@ -1,6 +1,15 @@
+# app.py
+from fastapi import FastAPI
+from mangum import Mangum
 
-def handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello, World!'
-    }
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+handler = Mangum(app)
